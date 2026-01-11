3f8a0cfb102cfddd74126bac7c659c6b5140ce87
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-11 16:58:40 -0300

    feat(auth): initial login and register page structure
    - Create login controller to render the view
    - Create login view with login and register forms
    - Implement basic UI with TailwindCSS

---
+++ views/login.view.php
@@ -0,0 +1,27 @@
+<div class="mt-6 grid grid-cols-2 gap-2">
+  <div class="border border-stone-700 rounded">
+    <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
+    <form class="space-y-2 space-x-4">
+      <label class="text-stone-500 mb-px">Email</label>
+      <input type="email" class="border-stone-800 bg-stone-900 text-sm border-2 rounded-md focus:outline-none px-2 py-1 w-full" placeholder="digite seu email" name="email" required>
+      <label class="text-stone-500 mb-px">Senha</label>
+      <input type="password" class="border-stone-800 bg-stone-900 text-sm border-2 rounded-md focus:outline-none px-2 py-1 w-full" placeholder="digite sua senha" name="senha" required>
+      <button type="submit" class="border-lime-800 bg-lime-900 px-4 py-2 rounded-md border border-2 hover:bg-lime-800">Login</button>
+    </form>
+
+  </div>
+  <div class="border border-stone-700 rounded">
+    <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Registro</h1>
+    <form class="space-y-2 space-x-4">
+      <label class="text-stone-500 mb-px">Email</label>
+      <input type="email" class="border-stone-800 bg-stone-900 text-sm border-2 rounded-md focus:outline-none px-2 py-1 w-full" placeholder="digite seu email" name="email" required>
+      <label class="text-stone-500 mb-px">Confirme seu email</label>
+      <input type="email" class="border-stone-800 bg-stone-900 text-sm border-2 rounded-md focus:outline-none px-2 py-1 w-full" placeholder="digite seu email" name="email_confirmacao" required>
+      <label class="text-stone-500 mb-px">Senha</label>
+      <input type="password" class="border-stone-800 bg-stone-900 text-sm border-2 rounded-md focus:outline-none px-2 py-1 w-full" placeholder="digite sua senha" name="senha" required>
+      <button type="reset" class="border-lime-800 bg-lime-900 px-4 py-2 rounded-md border border-2 hover:bg-lime-800">Cancelar</button>
+      <button type="submit" class="border-lime-800 bg-lime-900 px-4 py-2 rounded-md border border-2 hover:bg-lime-800">Registrar</button>
+    </form>
+
+  </div>
+</div>
